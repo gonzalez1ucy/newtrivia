2f689fa2a1623635a94ea19fa13ea95457b5198e
Lucy Gonzalez <lucygonzalez@Lucys-MacBook-Pro.local>
2020-01-22 18:31:36 -0500

    First push to repo

---
+++ src/components/Answer.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import components
+
+
+class Answer extends Component {
+  
+  render() {
+    return (
+      <div> 
+             6
+      </div>
+    );
+  }
+}
+
+export default Answer;
---
+++ src/components/Question.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+// import components
+
+
+class Question extends Component {
+  constructor(props) {
+
+  }
+  render() {
+    return (
+      <div>
+        How many DUI'S did Lindsey Lohan get in the early 2000's?!
+       
+      </div>
+    );
+  }
+}
+//Add your QuestionText, Reset Button and AnswerButtons here.  
+export default Question;
