be278b8258a7a0639530f0fb1480cbb9e54e2459
Lucy Gonzalez <lucygonzalez@Lucys-MacBook-Pro.local>
2020-02-12 18:07:26 -0500

    reading values out of questions array

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,24 +5,30 @@ import Answer from './Answer.jsx';
 // import components
 
 const QUESTIONS = [
-  {question: "Lindsey Lohan", answers: [
+  {question_text: "How many DUIS does Lindsey Lohan get in 2000's?", choices: [
     "1",
     "2",
-    "230"
-  ]}
+    "230",
+    "6"
+  ], correct_choice_index: 3},
+  {question_text: "How many Grammy's does Britney Spears have?", choices: [
+    "1",
+    "2",
+    "230",
+    "6"
+  ], correct_choice_index: 2}
 ]
 
 class App extends Component {
-
   render() {
     return (
       <div className="app">
         Trivia!
-        <Question question_text={"What color is "}/>
-        <Answer choices={"10"}/>
-        <Answer choices={"20"}/>
-        <Answer choices={"30"}/>
-        <Answer choices={"40"}/>
+        <Question question_text={QUESTIONS[0].question_text}/>
+        <Answer choices={QUESTIONS[0].choices[0]}/>
+        <Answer choices={QUESTIONS[0].choices[1]}/>
+        <Answer choices={QUESTIONS[0].choices[2]}/>
+        <Answer choices={QUESTIONS[0].choices[3]}/>
         </div>
     );
   }
