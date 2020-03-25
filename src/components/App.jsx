ed34e0261911704f6640758847b38461a2419683
Lucy Gonzalez <lucygonzalez@Lucys-MBP.home>
2020-03-25 18:31:10 -0400

    adding new code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,12 +5,16 @@ import Answer from './Answer.jsx';
 // import components
 
 const QUESTIONS = [
-  {question_text: "How many DUIS does Lindsey Lohan get in 2000's?", choices: [
-    "1",
-    "2",
-    "230",
-    "8",
-  ], correct_choice_index: 3},
+  {
+    question_text: "How many DUIS does Lindsey Lohan get in 2000's?",
+    choices: [
+      "1",
+      "2",
+      "230",
+      "8",
+    ],
+   correct_choice_index: 3
+  },
   {question_text: "How many Grammy's does Britney Spears have?", choices: [
     "1",
     "2",
@@ -22,28 +26,30 @@ const QUESTIONS = [
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { clicked: "no" };
+    this.state = { clicked: "no" , hello: "Welcome to Lindsey trivia!"};
+    
   }
 
-  handleClick(){
-    console.log('Hello!');
-    if(QUESTIONS[1]["choices"][3]) {
-      console.log('Here!!');
-      this.setState({
-        clicked: "YAY!"
-      });
+  handleClick(input){
 
-    }
+   if (input===QUESTIONS[0].choices[QUESTIONS[0].correct_choice_index]){
+    this.setState({
+      hello: "YAY!"
+    });
+   }else{this.setState({
+    hello: "NO MAYBE TRY AGAIN!"
+  });}
   };
   render() {
     return (
       <div className="app">
         Trivia!
-        <Question question_text={QUESTIONS[1].question_text}/>
-        <Answer handleClick={() => this.handleClick()} choices={QUESTIONS[0].choices[0]}/>
-        <Answer handleClick={() => this.handleClick()} choices={QUESTIONS[0].choices[1]}/>
-        <Answer handleClick={() => this.handleClick()} choices={QUESTIONS[0].choices[2]}/>
-        <Answer handleClick={() => this.handleClick()} choices={QUESTIONS[0].choices[3]}/>
+        <Question question_text={QUESTIONS[0].question_text}/>
+        <Answer handleClick={() => this.handleClick(QUESTIONS[0].choices[0])} choices={QUESTIONS[0].choices[0]}/>
+        <Answer handleClick={() => this.handleClick(QUESTIONS[0].choices[1])} choices={QUESTIONS[0].choices[1]}/>
+        <Answer handleClick={() => this.handleClick(QUESTIONS[0].choices[2])} choices={QUESTIONS[0].choices[2]}/>
+        <Answer handleClick={() => this.handleClick(QUESTIONS[0].choices[3])} choices={QUESTIONS[0].choices[3]}/>
+        {this.state.hello}
         </div>
     );
   }
